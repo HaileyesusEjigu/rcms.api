35b8ea1688e367d80a8d21133a679902634b9b9c
HaileyesusEjigu <haileyesusejigu9@gmail.com>
2025-05-06 10:39:00 +0300

    rumour update api created

--- controllers/rumorsController.js
+++ controllers/rumorsController.js
@@ -1,4 +1,5 @@
 const rummers =[ {
+   id:1,
     name: "measels out break",
     sign: " fever,rush, vomiting",
     description: "50 people dead by measels out break around hawassa",
@@ -10,7 +11,7 @@ const rummers =[ {
     number_of_death: 50,
     reporting_date: "10/02/2025",
   },
-  {
+  { id:2,
     name: "malaria out break",
     sign: " fever, vomiting",
     description: "10 people dead by measels out break around hawassa",
@@ -23,6 +24,7 @@ const rummers =[ {
     reporting_date: "10/03/2025",
   },
   {
+    id:3,
     name: "cholera out break",
     sign: " fever, vomiting, diarrhea",
     description: "15 people dead by measels out break around hawassa",
@@ -40,6 +42,39 @@ const getAllRumours = (req, res) => {
   res.json(rummers);
 };
 
+const getRomurById = (req,res)=>{
+  const id = req?.params.id
+  const rumour = rummers.find((rum)=>rum.id == id)
+  if(!rumour){
+    return res.status(404).json({"message":"rumour not found"})
+  }
+  res.json(rumour)
+}
+
+const updateRumourById = (req, res) => {
+  const id = req.params.id;
+  const rummerUpdate = req.body;
+  const rumour = rummers.find((rum) => rum.id == id);
+  if (!rumour) {
+    return res.status(404).json({ message: "rummour not found" });
+  }
+  rumour.name = rummerUpdate.name ? rummerUpdate.name : rumour.name;
+  rumour.sign = rummerUpdate.sign;
+  rumour.description = rummerUpdate.description;
+  rumour.region = rummerUpdate.region;
+  rumour.zone = rummerUpdate.zone;
+  rumour.kebele = rummerUpdate.kebele;
+  rumour.number_of_case = rummerUpdate.number_of_case;
+  rumour.number_of_death = rummerUpdate.number_of_death;
+  rumour.reporting_date = rummerUpdate.reporting_date;
+
+  res.json({ message: "successfully updated", data: rumour });
+};
+
+
+
 module.exports = {
   getAllRumours,
+  getRomurById,
+  updateRumourById
 };
--- routes/rumorsRoute.js
+++ routes/rumorsRoute.js
@@ -1,7 +1,10 @@
-const express = require('express')
+const express = require('express');
+const { getRomurById } = require('../controllers/rumorsController');
 rummersController = require("../controllers/rumorsController");
 const rummersRouter = express.Router();
 
 rummersRouter.get("/", rummersController.getAllRumours);
+rummersRouter.get("/:id", rummersController.getRomurById)
+rummersRouter.put("/:id", rummersController.updateRumourById)
 
 module.exports = rummersRouter;
